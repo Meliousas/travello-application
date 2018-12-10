1d02be969099da7da384361486db6ae766002637
Meliousas <meliousas@gmail.com>
2018-12-10 09:34:55 +0100

    Update Trip.java

--- src/main/java/com/example/travello/entity/Trip.java
+++ src/main/java/com/example/travello/entity/Trip.java
@@ -26,7 +26,7 @@ import java.util.Set;
 public class Trip {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+   // @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotNull
